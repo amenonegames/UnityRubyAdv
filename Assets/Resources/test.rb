11db88c2d0c7230b546a6ca404c0f40d26b174f5
amenonegames <100146683+amenonegames@users.noreply.github.com>
2024-12-28 06:34:52 +0900

    managerを作り、書き方を整理

--- Assets/Resources/test.rb
+++ Assets/Resources/test.rb
@@ -10,20 +10,72 @@ def o(opMessage)
     cmd :option, options: opMessage
 end
 
-def yesResponce()
-    t("Yes? Really?")
-end
+class DialogueManager
+    def initialize
+      @nodes = {}
+      @state = state
+    end
+  
+    def add_node(name, &block)
+      @nodes[name] = block
+    end
+  
+    def jump_to(name)
+      if @nodes[name]
+        @nodes[name].call
+      else
+        puts "Node #{name} not found!"
+      end
+    end
+  
+    def set_state(key, value)
+        @state[key] = value
+    end
 
-debug("start")
-t("please press continue button")
+    def get_state(key)
+    @state[key]
+    end
 
-o(["Yes", "No"])
+    # 判定用メソッドを改良
+    def is_state?(key, value)
+        @state[key]&.is?(value) # nilガードを追加
+    end
 
-if state[:result].is?("Yes") #=> true
-    yesResponce()
-else 
-    t("No? Really?")
-end
+  end
+  
+  # ダイアログの定義
+  manager = DialogueManager.new
+  
+  manager.add_node(:start) do
+    t "Start: Please press continue button"
+    manager.jump_to(:option)
+  end
+  
+  manager.add_node(:option) do
+    o ["Yes", "No"]
+    result = manager.is_state?(:result, "Yes")  #異常動作
+    result = state[:result].is?("Yes") #正常動作
+    if result #=> true
+        manager.jump_to(:yes_response)
+    else 
+        manager.jump_to(:no_response)
+    end
+  end
+  
+  manager.add_node(:yes_response) do
+    t "Yes? Really?"
+    manager.jump_to(:end)
+  end
 
-t("end")
-
+  manager.add_node(:no_response) do
+    t "No? Really?"
+    manager.jump_to(:end)
+  end
+  
+  manager.add_node(:end) do
+    t "End of the dialogue."
+  end
+  
+  # ダイアログの開始
+  manager.jump_to(:start)
+  
